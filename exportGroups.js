9aada3388c8802fe08aeb853929036431bde803d
Kate <katekuttler@gmail.com>
2016-01-15 06:50:25 -0800

    Replace left-over tabs with spaces

--- exportGroups.js
+++ exportGroups.js
@@ -27,7 +27,7 @@ var MAX_SCALE = 3;
 // so I got this somewhat confusing code by recording my actions 
 // with Photoshop's Scripting Listener Plug-in
 function resizeActiveDocument(newWidth) {
-	var idImgS = charIDToTypeID("ImgS");
+    var idImgS = charIDToTypeID("ImgS");
     var desc4 = new ActionDescriptor();
     var idWdth = charIDToTypeID("Wdth");
     var idPxl = charIDToTypeID("#Pxl");
@@ -40,13 +40,13 @@ function resizeActiveDocument(newWidth) {
     var idIntp = charIDToTypeID( "Intp" );
     var idautomaticInterpolation = stringIDToTypeID( "automaticInterpolation" );
     desc4.putEnumerated( idIntr, idIntp, idautomaticInterpolation );
-	executeAction( idImgS, desc4, DialogModes.NO );
+    executeAction( idImgS, desc4, DialogModes.NO );
 }
 
 // dups active document, resizes it to the given width, and makes the dupped doc the active doc
 function dupDocumentAtSize(newWidth) {
-	app.activeDocument = app.documents[app.activeDocument.name].duplicate();
-	resizeActiveDocument(newWidth);
+    app.activeDocument = app.documents[app.activeDocument.name].duplicate();
+    resizeActiveDocument(newWidth);
 }
 
 // actually saves the file as a png with the given name and destination
@@ -95,7 +95,7 @@ function saveFile(fileName, scale, destination) {
 // also assumes that the document that the layerSet belongs to is the active document
 function exportLayerSet(layerSet, scale, destination) {
     layerSet.visible = true;
-	
+    
     // make all layers in the current layerSet visible except size
     // get bounds of layer, if size layer exists
     var bounds = [];
@@ -160,7 +160,7 @@ function exportLayerSets(exportOptions) {
         
         var newWidth = app.activeDocument.width / sourceArtScale * currScale; // make each one 1x and then multipy by current scale factor
         dupDocumentAtSize(newWidth); 
-		
+        
         for( var i = 0; i < app.activeDocument.layerSets.length; i++) {
             // First check if we should skip exporting the current layer set
             if (exportOptions.visibleOnly) { 
@@ -182,20 +182,20 @@ function exportLayerSets(exportOptions) {
         }
         app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
         app.activeDocument = originalDoc;
-	}		
+    }       
 }
 
 // tries to get export info from photoshop registry first; if not there, initializes defaults
 function getExportOptions() {
-	try {
-		var d = app.getCustomOptions(appDescriptor);
-		exportOptions = getExportOptionsFromDescriptor(d);
-	}
-	catch(e) {
+    try {
+        var d = app.getCustomOptions(appDescriptor);
+        exportOptions = getExportOptionsFromDescriptor(d);
+    }
+    catch(e) {
         exportOptions.destination = new String("");
         exportOptions.visibleOnly = false;
         exportOptions.scale = 1;
-	}
+    }
     return exportOptions;
 }
 
@@ -207,15 +207,15 @@ function getExportOptionsFromDescriptor(desc) {
         var key = app.typeIDToStringID(descKey);
         
         switch (type) {
-			case DescValueType.BOOLEANTYPE:
-				exportOptions[key] = desc.getBoolean(descKey);
-				break;
-			case DescValueType.STRINGTYPE:
-				exportOptions[key] = desc.getString(descKey);
-				break;
-			case DescValueType.DOUBLETYPE:
-				exportOptions[key] = desc.getDouble(descKey);
-				break;
+            case DescValueType.BOOLEANTYPE:
+                exportOptions[key] = desc.getBoolean(descKey);
+                break;
+            case DescValueType.STRINGTYPE:
+                exportOptions[key] = desc.getString(descKey);
+                break;
+            case DescValueType.DOUBLETYPE:
+                exportOptions[key] = desc.getDouble(descKey);
+                break;
         }
     }
     return exportOptions;
@@ -240,10 +240,10 @@ function getDescriptorFromExportOptions(exportOptions) {
     return desc;
 }
 
-function main() {	
-	if (typeof(app) === "undefined" || typeof(app.documents) === "undefined" || app.documents.length <= 0) {
+function main() {   
+    if (typeof(app) === "undefined" || typeof(app.documents) === "undefined" || app.documents.length <= 0) {
         alert(openDocAlert);
-    	return 'cancel'; 
+        return 'cancel'; 
     }
 
     var exportOptions = getExportOptions();
@@ -271,7 +271,7 @@ function main() {
     dialog.panelDest = dialog.add("panel", undefined, destinationLabel);
     dialog.panelDest.alignment = 'fill';
     dialog.panelDest.orientation = 'row';
-	dialog.panelDest.alignChildren = 'left';
+    dialog.panelDest.alignChildren = 'left';
 
     dialog.destination = dialog.panelDest.add("edittext", undefined, exportOptions.destination);
     dialog.destination.preferredSize.width = 500;
@@ -292,14 +292,14 @@ function main() {
         }
     }
 
-	// add cancel and ok buttons
-	dialog.groupBottom = dialog.add("group");
-	dialog.exportButton = dialog.groupBottom.add("button", undefined, exportLabel);
-	dialog.cancelButton = dialog.groupBottom.add("button", undefined, cancel);
-	dialog.cancelButton.onClick = function() { 
-		dialog.close(); 
-	}
-	dialog.exportButton.onClick = function() {
+    // add cancel and ok buttons
+    dialog.groupBottom = dialog.add("group");
+    dialog.exportButton = dialog.groupBottom.add("button", undefined, exportLabel);
+    dialog.cancelButton = dialog.groupBottom.add("button", undefined, cancel);
+    dialog.cancelButton.onClick = function() { 
+        dialog.close(); 
+    }
+    dialog.exportButton.onClick = function() {
         
         if (dialog.destination.text.length <= 0) {
             alert("Please choose a valid folder to export to.");                
@@ -314,18 +314,18 @@ function main() {
             
         exportLayerSets(exportOptions);
         dialog.close();
-	}
+    }
 
-	dialog.panelNotes = dialog.add("panel", undefined, notes);
-	dialog.panelNotes.alignment = 'fill';
+    dialog.panelNotes = dialog.add("panel", undefined, notes);
+    dialog.panelNotes.alignment = 'fill';
     dialog.panelNotes.orientation = 'column';
-	dialog.panelNotes.alignChildren = 'left';
-	dialog.panelNotes.add("statictext", undefined, scaleNote);
-	dialog.panelNotes.add("statictext", undefined, ignoreGroupNote);
-	dialog.panelNotes.add("statictext", undefined, emptySpaceNote);
-	dialog.panelNotes.add("statictext", undefined, consistentSizeNote);
+    dialog.panelNotes.alignChildren = 'left';
+    dialog.panelNotes.add("statictext", undefined, scaleNote);
+    dialog.panelNotes.add("statictext", undefined, ignoreGroupNote);
+    dialog.panelNotes.add("statictext", undefined, emptySpaceNote);
+    dialog.panelNotes.add("statictext", undefined, consistentSizeNote);
 
-	var result = dialog.show();
+    var result = dialog.show();
 }
 
 main();
