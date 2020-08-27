6ced5fbd34c70fc40ce9b53d9a7fc497469f2afc
Kate Phillips <katekuttler@hotmail.com>
2020-08-27 12:30:51 -0700

    Split up main a bit

--- convert-to-booklet.js
+++ convert-to-booklet.js
@@ -1,4 +1,4 @@
-/* This script converts a series of .jpgs into another series of .jpgs that can 
+﻿/* This script converts a series of .jpgs into another series of .jpgs that can 
 be printed as a booklet. Each original .jpg is a spread in a book. The script
 recombines the image halves to allow you to print the resulting .jpgs double-
 sided, and fold into a booklet.*/
@@ -11,24 +11,21 @@ const previousFolderID = stringIDToTypeID("previousFolderKey");
 
 function main() {
     
-    dialog = new Window("dialog", title);
-    titleSelectDirectory = "Select the source directory"
-
-    var previouslySelectedFolder = getPreviouslySelectedFolder(); 
-    var selectedFolder = Folder.selectDialog(titleSelectDirectory, previouslySelectedFolder);
-    if (selectedFolder == null) {
-        alert("You need to select a folder of image spreads to run this script");
+    var selectedFolder = selectFolder();
+    var spreads = getCoverAndSpreads(selectedFolder);
+    if (spreads == null)
         return;
-    }
-    saveSelectedFolder(selectedFolder.fsName);
-    
+}
+
+/* returns an array of files; the first file is the cover and the rest are the interior spreads */
+function getCoverAndSpreads(selectedFolder) {
     var files = selectedFolder.getFiles();
     var cover = null;
     var spreads = [];
-    var indexInSpreads = 0;
+    var indexInSpreads = 1;
     for (var i = 0; i < files.length; i++) {
         var fileName = files[i].name.toLowerCase();
-        if (fileName.length > 4 && fileName.slice(0,5) == "cover") {
+        if (fileName.length > 4 && fileName.slice(0,5) == "cover" && fileName.indexOf(".jpg") > 0) {
             cover = files[i];
             continue;
         }
@@ -39,9 +36,24 @@ function main() {
     }
     if (cover == null) {
         alert("You need a cover image to run this script; it should be named 'cover'");
+        return null;
+    }
+    spreads[0] = cover;
+    return spreads;
+}
+
+function selectFolder() {
+    dialog = new Window("dialog", title);
+    titleSelectDirectory = "Select the source directory"
+
+    var previouslySelectedFolder = getPreviouslySelectedFolder(); 
+    var selectedFolder = Folder.selectDialog(titleSelectDirectory, previouslySelectedFolder);
+    if (selectedFolder == null) {
+        alert("You need to select a folder of image spreads to run this script");
         return;
     }
-    
+    saveSelectedFolder(selectedFolder.fsName);
+    return selectedFolder;
 }
 
 function getPreviouslySelectedFolder() {
