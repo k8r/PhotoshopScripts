e7ee773bf4fda34360a8b6c76b4b8079a2af0d44
Kate Phillips <katekuttler@hotmail.com>
2020-08-27 11:58:48 -0700

    Find the cover and spreads for the booklet

--- convert-to-booklet.js
+++ convert-to-booklet.js
@@ -6,16 +6,62 @@ sided, and fold into a booklet.*/
 var title = "Convert to Printable Booklet";
 var dirLabel = "Directory";
 
+const persistentSettingsID = stringIDToTypeID("persistentSettings");
+const previousFolderID = stringIDToTypeID("previousFolderKey");
+
 function main() {
     
     dialog = new Window("dialog", title);
     titleSelectDirectory = "Select the source directory"
 
-    var defaultFolder = "~";
-    var selectedFolder = Folder.selectDialog(titleSelectDirectory, defaultFolder);
-    if ( selectedFolder != null ) {
-        /* do work here */
+    var previouslySelectedFolder = getPreviouslySelectedFolder(); 
+    var selectedFolder = Folder.selectDialog(titleSelectDirectory, previouslySelectedFolder);
+    if (selectedFolder == null) {
+        alert("You need to select a folder of image spreads to run this script");
+        return;
     }
+    saveSelectedFolder(selectedFolder.fsName);
+    
+    var files = selectedFolder.getFiles();
+    var cover = null;
+    var spreads = [];
+    var indexInSpreads = 0;
+    for (var i = 0; i < files.length; i++) {
+        var fileName = files[i].name.toLowerCase();
+        if (fileName.length > 4 && fileName.slice(0,5) == "cover") {
+            cover = files[i];
+            continue;
+        }
+        if (fileName.indexOf(".jpg") > 0) {
+            spreads[indexInSpreads] = files[i];
+            indexInSpreads++;
+        }
+    }
+    if (cover == null) {
+        alert("You need a cover image to run this script; it should be named 'cover'");
+        return;
+    }
+    
+}
+
+function getPreviouslySelectedFolder() {
+    var selectedFolder = "~";
+    var settings;
+    try {
+        settings = getCustomOptions(persistentSettingsID);
+    } catch (e) {
+        settings = new ActionDescriptor();
+    }
+    
+    if (settings.hasKey(previousFolderID))
+        selectedFolder = settings.getString(previousFolderID);
+    return selectedFolder;
+}
+
+function saveSelectedFolder(selectedFolder) {
+    var selectedFolderDescriptor = new ActionDescriptor();
+    selectedFolderDescriptor.putString(previousFolderID, selectedFolder);
+    putCustomOptions(persistentSettingsID, selectedFolderDescriptor, true);
 }
 
 main();
